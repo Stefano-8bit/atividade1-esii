6ee25af9ed971080ffbae472480142aed498355c
lucaslettnin <luketlettnin@gmail.com>
2024-04-09 11:54:41 -0300

    adicionado a função subtração

--- calculator.py
+++ calculator.py
@@ -7,16 +7,7 @@ def sum(num1, num2):
     return num1 + num2
 print(f"Soma dos numeros {num1} e {num2} Resulta em: ", int(sum(num1, num2)))
 
-// retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
-
-// retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
-
-// retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
-
-// retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
-
-console.log(sum(v1, v2));
+# retorna o valor da subtração entre os dois parâmetros
+def sub(num1, num2):
+    return num1 - num2
+print(f"Subtração dos numeros {num1} e {num2} Resulta em: ", int(sub(num1, num2)))
