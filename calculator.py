2c0227e7be6c9279874d19a75f360c63ee275784
Luis Felipe Jr <felipequineper@hotmail.com>
2024-04-09 18:28:28 -0300

    Adicionado função de multiplicação

--- calculator.py
+++ calculator.py
@@ -12,9 +12,16 @@ def sub(num1, num2):
     return num1 - num2
 print(f"Subtração dos numeros {num1} e {num2} Resulta em: ", int(sub(num1, num2)))
 
+
 def div(num1, num2):
     if num2 == 0:
         print("❌Não é possivel dividir por zero❌")
         return False
     return num1 / num2
 print(f"Divisão dos numeros {num1} e {num2} Resulta em: ", int(div(num1, num2)))
+
+# retorna o valor da multiplicação de dois parâmetros
+def mult(num1, num2):
+    return num1 * num2
+print(f"Multiplicação dos numeros {num1} e {num2} Resulta em: ", int(mult(num1, num2)))
+
