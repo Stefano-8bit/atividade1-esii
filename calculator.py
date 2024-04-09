c7204e9df4be01225a031443b81d1a107418ca3c
ViktorVieira05 <viktor.planasul@gmail.com>
2024-04-09 11:58:08 -0300

    Adicionado funçao divisão

--- calculator.py
+++ calculator.py
@@ -11,3 +11,10 @@ print(f"Soma dos numeros {num1} e {num2} Resulta em: ", int(sum(num1, num2)))
 def sub(num1, num2):
     return num1 - num2
 print(f"Subtração dos numeros {num1} e {num2} Resulta em: ", int(sub(num1, num2)))
+
+def div(num1, num2):
+    if num2 == 0:
+        print("❌Não é possivel dividir por zero❌")
+        return False
+    return num1 / num2
+print(f"Divisão dos numeros {num1} e {num2} Resulta em: ", int(div(num1, num2)))
